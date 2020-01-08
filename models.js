919c9dfb1a1a72d16d87598f2c1987948a8be08d
eva montgomery <eva.m.montgomery@gmail.com>
2020-01-08 13:28:04 -0500

    adds ternary operator

--- models.js
+++ models.js
@@ -7,10 +7,28 @@ function all() {
 };
 
 function create(name, joyVal) {
-    let givesJoy = false;
-    if (joyVal) {
-        givesJoy = true;
-    }
+    // if I wanted to store 'on' or 'off'
+    // instead of 'on' or undefinded
+    // let givesJoy = joyVal || 'off';
+
+    // I want to test if 'joyVal' is truthy
+    // if it is, I want 'true'
+    // else, I want 'false'
+
+    // let givesJoy = false;
+    // if (joyVal) {
+    //     givesJoy = true;
+    // }
+    
+    // this is the ternary operator:
+    let givesJoy = joyVal? true: false;
+    // let givesJoy = joyVal? '🍺' : '🏄🏼‍♂️';
+
+
+    // it tests a value for truthy-ness
+    // if the value is truthy, resolves to the thing on the left of the colon
+    // if the value is false, resolves to the right thing of the colin
+
     const newItem = {
         name, 
         givesJoy
